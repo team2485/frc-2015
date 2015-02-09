f0ae79bf568eab479caabf49f7cf212ab24fc759
Ben Clark <rclark2017@francisparker.org>
2015-02-09 13:37:37 -0800

    wrote Ratchet System class including solenoids for the hook and the bottom tote rest

---
+++ src/org/usfirst/frc/team2485/robot/RatchetSystem.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team2485.robot;
+
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class RatchetSystem {
+
+	private Solenoid hook, bottomRest;
+	
+	public RatchetSystem(Solenoid hook, Solenoid bottomRest) {
+		this.hook		= hook;
+		this.bottomRest	= bottomRest;
+	}
+	
+	public RatchetSystem(int hookPort, int bottomRestPort) {
+		this(new Solenoid(hookPort), new Solenoid(bottomRestPort));
+	}
+	
+	public void releaseToteStack() {
+		hook.set(true);
+		bottomRest.set(true);
+	}
+	
+	public void setDefaultRatchetPosition() {
+		hook.set(false);
+		bottomRest.set(false);
+	}
+}
