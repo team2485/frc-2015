fc356441894f4c56db47fb6599fd26fdcd8e5a92
Patrick Wamsley <pwamsley2015@francisparker.org>
2015-03-10 22:11:42 -0700

    Cleared up documentation.

--- src/org/usfirst/frc/team2485/util/ThresholdHandler.java
+++ src/org/usfirst/frc/team2485/util/ThresholdHandler.java
@@ -16,7 +16,7 @@ public class ThresholdHandler {
 	 */
 	
 	public static final double STANDARD_THRESHOLD = 0.1; 
-	@Deprecated
+	
 	public static double handleThreshold(double val, double threshold) {
 		return (Math.abs(val) > Math.abs(threshold)) ? (val / Math.abs(val) * (Math.abs(val) - threshold) / (1 - threshold)) : 0.0;
 	}
@@ -29,17 +29,18 @@ public class ThresholdHandler {
 	}
 	
 	/**
-	 * Thresholds and scales values linearly: <code>((max - min) / (1 - t)) * (v - t) + min;</code> where <code>(max - min) / (1 - t)</code> is the slope.
+	 * Thresholds and scales values linearly.                                                                      
+	 * <code>((max - min) / (1 - t)) * (v - t) + min;</code> where <code>(max - min) / (1 - t)</code> is the slope.
 	 * Derived from fact that an input value of the threshold should return min : ( <code>f(threshold) = min</code> )
 	 * and an input value of 1 should return max : ( <code> f(1) = max </code> ) 
 	 *  
-	 * @param val = value from input
-	 * @param threshold = deadband threshold 
-	 * @param absoluteMin = absolute value of min range
-	 * @param absoluteMax = absolute value of max range 
+	 * @param val = raw input from controllers/joysticks
+	 * @param threshold = deadband threshold  (values less than this are ignored) 
+	 * @param absoluteMin = absolute value of min range (must be greater than 0)
+	 * @param absoluteMax = absolute value of max range (must be less than 1) 
 	 * 
 	 * @return Corrected input from joystick. If input is below threshold, 0 is returned. 
-	 * 		   If not, input is scaled between [min, max]. 
+	 * 		   If not, input is scaled between (min, max). 
 	 * 
 	 */
 	public static double deadbandAndScale(double val, double threshold, double absoluteMin, double absoluteMax) {
