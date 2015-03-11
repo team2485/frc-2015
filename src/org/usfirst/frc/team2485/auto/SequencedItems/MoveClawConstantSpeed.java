1800f854dc2665b87e745c9df17fc22a9efe6040
Patrick Wamsley <pwamsley2015@francisparker.org>
2015-03-11 14:11:05 -0700

    Refactored and fixed behavior of MoveClapperConstantSpeed
    Now matches MoveClawConstantSpeed and has fixed logic.

--- src/org/usfirst/frc/team2485/auto/SequencedItems/MoveClawConstantSpeed.java
+++ src/org/usfirst/frc/team2485/auto/SequencedItems/MoveClawConstantSpeed.java
@@ -7,6 +7,7 @@ import org.usfirst.frc.team2485.robot.Robot;
  * Sets the Claw to move at a set speed. Does not time out or stop the claw unless <code>speed = 0</code>. 
  * 
  * @author Patrick Wamsley
+ * @see org.usfirst.frc.team2485.auto.SequencedItemsMoveClapperVertically
  */
 public class MoveClawConstantSpeed implements SequencedItem {
 	
